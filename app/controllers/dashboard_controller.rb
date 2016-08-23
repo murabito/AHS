28dc2908dbeb7d1a9d61d3548dfdd0170573ff2e
Christine Murabito <christine.murabito@gmail.com>
2016-08-23 07:10:25 -0400

    Add initial controller action for patient search

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,14 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   
   def searches
+    response = RedoxApi::Core::RequestService.patient_query("101-01-0001")
+  end
+
+  def patient_search
+  end
+
+  def retrieve_patient
+    ssn = params["ssn"]
+    redirect_to '/patient'
   end
 end
