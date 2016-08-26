b4c79e6c15ba5979cfc9e686da6c8055215fef42
Christine Murabito <christine.murabito@gmail.com>
2016-08-26 17:27:41 -0400

    Retrieve patient id from query response data

--- app/lib/redox_api/patient.rb
+++ app/lib/redox_api/patient.rb
@@ -1,9 +1,21 @@
 module RedoxApi
   class Patient
     attr_reader :data
+    attr_reader :id
 
     def initialize(data={})
       @data = data
+      @id = patient_id
+    end
+
+    def patient_id
+      identifiers = @data["Identifiers"]
+
+      nist_id = ''
+      identifiers.each do |identifier|
+        nist_id = identifier["ID"] if identifier["IDType"] == 'NIST'
+      end
+      nist_id
     end
   end
 end
