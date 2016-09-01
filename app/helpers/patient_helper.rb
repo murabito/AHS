da9757f02a626a1d3725b4d3454087bc10cae490
Christine Murabito <christine.murabito@gmail.com>
2016-09-01 10:36:54 -0400

    Add medications table

--- app/helpers/patient_helper.rb
+++ app/helpers/patient_helper.rb
@@ -2,4 +2,18 @@ module PatientHelper
   def start_date(medication)
     Date.strptime(medication["StartDate"], '%Y-%m-%d')
   end
+
+  def end_date(medication)
+    Date.strptime(medication["EndDate"], '%Y-%m-%d') if !!medication["EndDate"]
+  end
+
+  def status(medication)
+    if end_date(medication).blank?
+      return 'Status is unavailable'
+    elsif DateTime.now <= end_date(medication)
+      return 'Active'
+    else
+      return 'Inactive'
+    end
+  end
 end
