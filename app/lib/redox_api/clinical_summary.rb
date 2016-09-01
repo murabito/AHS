08d5c17678a249950dfbfcf8450bf095f8ffdf74
Christine Murabito <christine.murabito@gmail.com>
2016-09-01 13:49:44 -0400

    Add social history section

--- app/lib/redox_api/clinical_summary.rb
+++ app/lib/redox_api/clinical_summary.rb
@@ -24,6 +24,18 @@ module RedoxApi
       self.data["Problems"]
     end
 
+    def social_history_observations
+      self.data["SocialHistory"]["Observations"]
+    end
+
+    def social_history_pregnancies
+      self.data["SocialHistory"]["Pregnancy"]
+    end
+
+    def social_history_tobacco_use
+      self.data["SocialHistory"]["TobaccoUse"]
+    end
+
     def has_allergies_data?
       !self.data["Allergies"].blank?
     end
@@ -43,5 +55,26 @@ module RedoxApi
     def has_problems_data?
       !self.data["Problems"].blank?
     end
+
+    def has_social_history_data?
+      !self.data["SocialHistory"].blank?
+    end
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
