40662f770cd6d76890f9597588c75cdb7d4029bc
Christine Murabito <christine.murabito@gmail.com>
2016-09-01 13:44:29 -0400

    Add effective date string helper method

--- app/helpers/patient_helper.rb
+++ app/helpers/patient_helper.rb
@@ -4,18 +4,33 @@ module PatientHelper
     problem["AgeAtOnset"]
   end
 
-  def start_date(medication)
-    Date.strptime(medication["StartDate"], '%Y-%m-%d')
+  def formatted_date(date)
+    return '' if date.blank?
+    Date.strptime(date, '%Y-%m-%d').to_s
   end
 
-  def end_date(medication)
-    Date.strptime(medication["EndDate"], '%Y-%m-%d') if !!medication["EndDate"]
+  def effective_date_str(start_date, end_date)
+    start_date = formatted_date(start_date)
+    end_date = formatted_date(end_date)
+    
+    if start_date.empty? && end_date.empty?
+      effective_dates = 'Unavailable'
+    elsif start_date.empty?
+      effective_dates = 'Unavailable' + ' -- ' + end_date
+    elsif end_date.empty?
+      effective_dates = start_date + ' -- ' + 'Unavailable'
+    else
+      effective_dates = start_date + ' -- ' + end_date
+    end
+    effective_dates
   end
 
   def status(medication)
-    if end_date(medication).blank?
-      return 'Status is unavailable'
-    elsif DateTime.now <= end_date(medication)
+    return 'Status is unavailable' if medication["EndDate"].blank?
+
+    end_date = Date.strptime(formatted_date(medication["EndDate"]))
+    
+    if DateTime.now <= end_date
       return 'Active'
     else
       return 'Inactive'
