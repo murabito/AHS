f45a9e346ad1b0661a5f0648070ab64b5df26c0d
Christine Murabito <christine.murabito@gmail.com>
2016-09-19 17:04:21 -0400

    Display only last 5 recent views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   
   def searches
-    @recent_views = RecentView.where(user_id: current_user.id)
+    @recent_views = RecentView.where(user_id: current_user.id).last(5)
   end
 end
