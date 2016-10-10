ec0469f11509b7d3d051c736fef947a689f015b3
Christine Murabito <christine.murabito@gmail.com>
2016-10-10 16:21:12 -0400

    Add saved views

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   
   def searches
     @recent_views = RecentView.where(user_id: current_user.id).order(updated_at: :desc).last(5)
+    @saved_views = RecentView.where(user_id: current_user.id).where(is_saved: true).order(updated_at: :desc).last(5)
   end
 end
