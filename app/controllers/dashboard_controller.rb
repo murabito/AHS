3f190ee16ebfcfd5f65b99cf954cb052f2111841
Christine Murabito <christine.murabito@gmail.com>
2016-10-10 16:06:26 -0400

    Sort recent views by updated date / time

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,6 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
   
   def searches
-    @recent_views = RecentView.where(user_id: current_user.id).last(5)
+    @recent_views = RecentView.where(user_id: current_user.id).order(updated_at: :desc).last(5)
   end
 end
