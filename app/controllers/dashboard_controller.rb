2d442a214d6af159a54628f91394b9712ffe3137
Christine Murabito <christine.murabito@gmail.com>
2016-11-07 15:16:56 -0500

    Respond to redox verification request

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,7 +4,12 @@ class DashboardController < ApplicationController
   def verify
     if verification_request?
       if verify_token
-        RedoxApi::Core::RequestService.request("POST", "/", headers: {challenge: params["challenge"]})
+        # RedoxApi::Core::RequestService.request("POST", "/", headers: {challenge: params["challenge"]})
+
+        request = RedoxApi::Core::RequestService.build_request('POST', '/verify', 
+          :body => { :challenge => params["challenge"] }.to_json,
+          :headers => { 'Content-Type' => 'application/json' } )
+
         redirect_to dashboard_path(challenge: params["challenge"])
       end
     end
