ae54b1bc61e3d93e0963fbc4fd8feca0ba2089cd
Christine Murabito <christine.murabito@gmail.com>
2016-11-07 13:13:28 -0500

    Verify redox challenge

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-  before_action :authenticate_user!, except: :verify
+  before_action :authenticate_user!, :except => [:verify]
   
   def verify
     if verification_request?
@@ -10,16 +10,18 @@ class DashboardController < ApplicationController
     end
   end
 
+  def searches
+    @recent_views = RecentView.where(user_id: current_user.id).order(updated_at: :desc).last(5)
+    @saved_views = RecentView.where(user_id: current_user.id).where(is_saved: true).order(updated_at: :desc).last(5)
+  end
+
+  private
+
   def verification_request?
     !!params["challenge"]
   end
 
   def verify_token
     params["verification-token"] == ENV["REDOX_SECRET"]
-  end
-
-  def searches
-    @recent_views = RecentView.where(user_id: current_user.id).order(updated_at: :desc).last(5)
-    @saved_views = RecentView.where(user_id: current_user.id).where(is_saved: true).order(updated_at: :desc).last(5)
   end
 end
